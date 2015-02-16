27b76bd6acf0dc6456ae905ae061cada46326de8
Paul Walsh <paulywalsh@gmail.com>
2015-02-16 15:18:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='jtskit',
     description='A utility library for working with JSON Table Schema in Python',
     long_description='A utility library for working with JSON Table Schema in Python',
-    version='0.1.1',
+    version='0.1.2',
     author='Open Knowledge Foundation',
     author_email='info@okfn.org',
     url='http://okfn.org',
