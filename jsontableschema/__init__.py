0ece39edf1f07038e7cf5143a199764a4ae5b84a
roll <roll@post.agency>
2016-09-07 14:36:21 +0300

    removed __all__ from init, reorganised

--- jsontableschema/__init__.py
+++ jsontableschema/__init__.py
@@ -4,19 +4,17 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .field import Field
 from .schema import Schema
-from .storage import Storage
+from .field import Field
 from .table import Table
+from .storage import Storage
+from .validate import validate
 from .infer import infer
-from .pushpull import push_resource, pull_resource
-from .validate import validate, validator
 from . import exceptions
 from . import types
+
+# Deprecated
+from .validate import validator
+from .pushpull import push_resource, pull_resource
 from . import model
 from . import storage
-
-
-__all__ = ['exceptions', 'types', 'model', 'utilities',
-           'compat', 'infer', 'validate', 'validator',
-           'storage', 'push_resource', 'pull_resource']
