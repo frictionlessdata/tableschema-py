6612ca7bfac29f14bcc2e911dd1b6910d67ec9f1
roll <eskarev@gmail.com>
2017-08-22 16:45:52 +0300

    Implemented pk/unique check on table.iter/read

--- tableschema/table.py
+++ tableschema/table.py
@@ -9,6 +9,7 @@ from tabulator import Stream
 from functools import partial
 from importlib import import_module
 from .schema import Schema
+from . import exceptions
 
 
 # Module API
@@ -64,11 +65,26 @@ class Table(object):
     def iter(self, keyed=False, extended=False, cast=True):
         """https://github.com/frictionlessdata/tableschema-py#schema
         """
+        unique_cache = _create_unique_fields_cache(self.schema) if self.schema else {}
         with self.__stream as stream:
             iterator = stream.iter(extended=True)
             iterator = self.__apply_processors(iterator, cast=cast)
             for row_number, headers, row in iterator:
-                self.__headers = self.__headers or headers
+
+                # Headers
+                if not self.__headers:
+                    self.__headers = headers
+
+                # Unique
+                for index, cache in unique_cache.items():
+                    if row[index] in cache:
+                        field_name = self.schema.fields[index].name
+                        message = 'Field "%s" duplicates in row "%s"'
+                        message = message % (field_name, row_number)
+                        raise exceptions.TableSchemaException(message)
+                    cache.add(row[index])
+
+                # Form
                 if extended:
                     yield (row_number, headers, row)
                 elif keyed:
@@ -129,7 +145,7 @@ class Table(object):
             storage.write(target, self.iter())
             return storage
 
-    # Internal
+    # Private
 
     def __apply_processors(self, iterator, cast=True):
 
@@ -144,3 +160,13 @@ class Table(object):
             iterator = processor(iterator)
 
         return iterator
+
+
+# Internal
+
+def _create_unique_fields_cache(schema):
+    cache = {}
+    for index, field in enumerate(schema.fields):
+        if field.constraints.get('unique') or field.name in schema.primary_key:
+            cache[index] = set()
+    return cache
