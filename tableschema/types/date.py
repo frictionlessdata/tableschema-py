f0b9be721a0377f23291dc5749ab820faead6d06
Pierre Dittgen <pierre.dittgen@jailbreak.paris>
2018-10-18 10:31:00 +0200

    Allow int and float to string and datetime to date

--- tableschema/types/date.py
+++ tableschema/types/date.py
@@ -14,24 +14,34 @@ from ..config import ERROR
 # Module API
 
 def cast_date(format, value, **options):
-    if not isinstance(value, date):
-        if not isinstance(value, six.string_types):
-            return ERROR
-        try:
-            if format == 'default':
-                value = datetime.strptime(value, _DEFAULT_PATTERN).date()
-            elif format == 'any':
-                value = parse(value).date()
-            else:
-                if format.startswith('fmt:'):
-                    warnings.warn(
-                        'Format "fmt:<PATTERN>" is deprecated. '
-                        'Please use "<PATTERN>" without "fmt:" prefix.',
-                        UserWarning)
-                    format = format.replace('fmt:', '')
-                value = datetime.strptime(value, format).date()
-        except Exception:
-            return ERROR
+    if isinstance(value, date):
+        return value
+
+    if isinstance(value, datetime):
+        value_time = value.time()
+        if value_time.hour == 0 and value_time.minute == 0 and value_time.second == 0:
+            return value.date()
+
+    if not isinstance(value, six.string_types):
+        return ERROR
+
+    # Parse string date
+    try:
+        if format == 'default':
+            value = datetime.strptime(value, _DEFAULT_PATTERN)
+        elif format == 'any':
+            value = parse(value).date()
+        else:
+            if format.startswith('fmt:'):
+                warnings.warn(
+                    'Format "fmt:<PATTERN>" is deprecated. '
+                    'Please use "<PATTERN>" without "fmt:" prefix.',
+                    UserWarning)
+                format = format.replace('fmt:', '')
+            value = datetime.strptime(value, format).date()
+    except Exception:
+        return ERROR
+
     return value
 
 
