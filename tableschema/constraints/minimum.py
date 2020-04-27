c2bab21cf899aba3377ab693da80bc17878639a8
roll <eskarev@gmail.com>
2020-04-27 13:47:15 +0300

    Handle non-finite decimal in mimimum/maximum

--- tableschema/constraints/minimum.py
+++ tableschema/constraints/minimum.py
@@ -4,12 +4,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import decimal
 
 # Module API
 
 def check_minimum(constraint, value):
     if value is None:
         return True
-    if value >= constraint:
-        return True
+    try:
+        if value >= constraint:
+            return True
+    except decimal.InvalidOperation:
+        # For non-finite numbers NaN, INF and -INF
+        # the constraint always is not satisfied
+        return False
     return False
